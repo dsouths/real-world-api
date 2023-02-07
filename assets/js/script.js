4deddbc33db1dc524fb75f90065fb65ba534f887
dsouths <dsouths@hotmail.com>
2023-02-07 15:51:46 +0000

    added three constants

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = "UFxRq8uwFF2jRrgVIVOogZXEGPw";
+const API_URL = "https://ci-jshint.herokuapp.com/api"
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
